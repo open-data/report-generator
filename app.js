open-data/report-generator

commit 0d23283cdcc6a327e10f5c3e7c9db198529b96f3

Fixed saved queries with spaces not working

app.js
@@ -62,7 +62,7 @@
         }
 
         $rootScope.ckanInstance = 'http://ndmckanq1.stcpaz.statcan.gc.ca';
-        $rootScope.query = wb.pageUrlParts.params.q || '';
+        $rootScope.query = decodeURI(wb.pageUrlParts.params.q) || '';
         $rootScope.maxResultsOptions = {
             20: 20,
             50: 50,

modules/display-fields.js
@@ -5,7 +5,7 @@
         var _this = this;
 
         function fromUrl() {
-            var displayFields = wb.pageUrlParts.params.fl,
+            var displayFields = decodeURI(wb.pageUrlParts.params.fl),
                 fields;
 
             if (displayFields) {

modules/organization.js
@@ -6,7 +6,7 @@
             _this = this;
 
         function fromURL() {
-            var organizations = wb.pageUrlParts.params.fq,
+            var organizations = decodeURI(wb.pageUrlParts.params.fq),
                 orgs = [];
 
             if (organizations) {

commit a76b9165354186b4879d575d249df9ae4fe5d996

Fixed various fields when no saved query is used
Problem introduced by 0d23283cdcc6a327e10f5c3e7c9db198529b96f3

app.js
@@ -66,7 +66,7 @@
         }
 
         $rootScope.ckanInstance = 'http://ndmckanq1.stcpaz.statcan.gc.ca';
-        $rootScope.query = decodeURI(wb.pageUrlParts.params.q) || '';
+        $rootScope.query = wb.pageUrlParts.params.q ? decodeURI(wb.pageUrlParts.params.q) : '';
         $rootScope.maxResultsOptions = {
             20: 20,
             50: 50,

modules/display-fields.js
@@ -5,12 +5,13 @@
         var _this = this;
 
         function fromUrl() {
-            var displayFields = decodeURI(wb.pageUrlParts.params.fl),
+            var displayFields = wb.pageUrlParts.params.fl,
                 fields;
 
             if (displayFields) {
+
                 fields = [].concat(_this.mandatoryFields);
-                displayFields.split(',').forEach(function(field) {
+                decodeURI(displayFields).split(',').forEach(function(field) {
                     if (fields.indexOf(field) === -1) {
                         fields.push(field);
                     }

modules/organization.js
@@ -6,11 +6,11 @@
             _this = this;
 
         function fromURL() {
-            var organizations = decodeURI(wb.pageUrlParts.params.fq),
+            var organizations = wb.pageUrlParts.params.fq,
                 orgs = [];
 
             if (organizations) {
-                organizations.split(',').forEach(function(o) {
+                decodeURI(organizations.split(',')).forEach(function(o) {
                     if (_this.organizations.indexOf(o) !== -1) {
                         orgs.push(o);
                     }